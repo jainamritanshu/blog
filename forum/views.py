0a0bb01a1e2c6633f12c3c888c911a22c70c72e9
jainamritanshu <jainamritanshu@gmail.com>
2016-03-03 16:08:43 +0530

    new file:   blog/__init__.py 	new file:   blog/__init__.pyc 	new file:   blog/settings.py 	new file:   blog/settings.pyc 	new file:   blog/urls.py 	new file:   blog/urls.pyc 	new file:   blog/wsgi.py 	new file:   blog/wsgi.pyc 	new file:   db.sqlite3 	new file:   forum/__init__.py 	new file:   forum/__init__.pyc 	new file:   forum/admin.py 	new file:   forum/admin.pyc 	new file:   forum/apps.py 	new file:   forum/migrations/0001_initial.py 	new file:   forum/migrations/0001_initial.pyc 	new file:   forum/migrations/__init__.py 	new file:   forum/migrations/__init__.pyc 	new file:   forum/models.py 	new file:   forum/models.pyc 	new file:   forum/templates/forum/detail.html 	new file:   forum/templates/forum/ques_list.html 	new file:   forum/templates/forum/your_ques.html 	new file:   forum/tests.py 	new file:   forum/views.py 	new file:   forum/views.pyc 	new file:   login/__init__.py 	new file:   login/__init__.pyc 	new file:   login/admin.py 	new file:   login/admin.pyc 	new file:   login/apps.py 	new file:   login/forms.py 	new file:   login/forms.pyc 	new file:   login/migrations/__init__.py 	new file:   login/migrations/__init__.pyc 	new file:   login/models.py 	new file:   login/models.pyc 	new file:   login/templates/base.html 	new file:   login/templates/home.html 	new file:   login/templates/registration/login.html 	new file:   login/templates/registration/register.html 	new file:   login/templates/registration/success.html 	new file:   login/tests.py 	new file:   login/views.py 	new file:   login/views.pyc 	new file:   manage.py

---
+++ forum/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import *
+from django.http import HttpResponse
+from django.http import Http404
+
+def ques_list(request):
+	latest_question_list = Question.objects.order_by('pub_date')
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'forum/your_ques.html', context)
+
+def detail(request, q_id):
+	try:
+		ques = Question.objects.get(pk=q_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'forum/detail.html', {'ques': ques})		
