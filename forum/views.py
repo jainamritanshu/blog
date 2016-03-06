1d5dbfed01ebc6be9bae9f6062681c480deeef41
Amritanshu jain <jainamritanshu@gmail.com>
2016-03-07 02:46:23 +0900

    forms for question done

--- forum/views.py
+++ forum/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from .models import *
 from django.http import HttpResponse
 from django.http import Http404
+from django.http import HttpResponseRedirect
+from forum.forms import *
 
 def ques_list(request):
 	latest_question_list = Question.objects.order_by('pub_date')
@@ -19,4 +21,17 @@ def detail(request, q_id):
 def ans_list(request):
 	latest_ans_list = Answer.objects.order_by('pub_date')	
 	a = {'latest_ans_list': latest_ans_list}
-	return render(request, 'forum/detail.html', a)
+	return render(request, 'forum/ans.html', a)
+
+def q_form(request):
+	if request.method == 'POST':
+		form = Question(request.POST)
+		if form.is_valid():
+			a = Question()
+			a.text = form.cleaned_data.get('y_ques', 'default1')
+			a.save()
+
+	else:
+		form = Question()
+	c_data = {'form': form}
+	return HttpResponseRedirect('ques_list.html', c_data)			
